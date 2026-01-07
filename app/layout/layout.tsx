567786f39cfda761bdc19f2d9d6e9cd4b69275c6
davinoctavian <davinoctavian@gmail.com>
2026-01-07 14:42:54 +0700

    menu layout

--- app/layout/layout.tsx
+++ app/layout/layout.tsx
@@ -3,7 +3,7 @@ import "./layout.css";
 
 export default function Layout() {
   return (
-    <div className="sky-root text-white">
+    <div className="sky-root text-white relative">
       <div className="nebula"></div>
       {/* Star layers */}
       <div className="stars stars-1"></div>
@@ -12,7 +12,7 @@ export default function Layout() {
       <div className="stars stars-4"></div>
 
       {/* Page content */}
-      <main className="relative z-10 max-w-4xl mx-auto p-6">
+      <main className="z-10 max-w-4xl mx-auto p-6">
         <Outlet />
       </main>
     </div>
--- app/menu/menu.tsx
+++ app/menu/menu.tsx
@@ -1,34 +1,57 @@
 import { NavLink } from "react-router";
+import "./menu.css";
 
 export function Menu() {
   return (
-    <nav>
-      {resources.map(({ link, name }) => (
-        <NavLink to={link}>{name}</NavLink>
-      ))}
+    <nav className="solar-system">
+      {resources.map(({ link, name, alias }) => {
+        const animationDelay = Math.random() * 42;
+        return (
+          <div
+            key={alias}
+            className={`orbit ${alias}`}
+            style={{ animationDelay: `-${animationDelay}s` }}
+          >
+            <NavLink to={link} className="planet">
+              <span className="planet-circle"></span>
+              <span
+                className="planet-label"
+                style={{ animationDelay: `-${animationDelay}s` }}
+              >
+                {name}
+              </span>
+            </NavLink>
+          </div>
+        );
+      })}
     </nav>
   );
 }
 
 const resources = [
   {
-    link: "/",
-    name: "Home",
-  },
-  {
     link: "/profile",
     name: "Profile",
+    alias: "mercury",
   },
   {
     link: "/about",
     name: "About",
+    alias: "venus",
+  },
+  {
+    link: "/",
+    name: "Home",
+    alias: "earth",
   },
   {
     link: "/project",
     name: "Project",
+    alias: "mars",
   },
   {
     link: "/cv",
     name: "CV",
+    alias: "jupiter",
   },
 ];
